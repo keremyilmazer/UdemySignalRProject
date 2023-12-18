92dd85da323c71c57fe72a174f249bc2adf18599
Kerem <keremyilmazerrr@hotmail.com>
2023-12-19 01:13:44 +0300

    Entitylerin devamı

---
+++ SignalRApi/DAL/Entities/Feature.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalRApi.DAL.Entities
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public int Title1 { get; set; }
+        public int Description1 { get; set; }
+        public int Title2 { get; set; }
+        public int Description2 { get; set; }
+        public int Title3 { get; set; }
+        public int Description3 { get; set; }
+    }
+}
