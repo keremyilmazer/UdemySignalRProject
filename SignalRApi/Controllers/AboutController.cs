08ef17c2f79335d07c4ea46217c9cdf305bc07fb
Kerem <keremyilmazerrr@hotmail.com>
2023-12-30 17:01:58 +0300

    Hakkımda İşlemleri

--- SignalRApi/Controllers/AboutController.cs
+++ SignalRApi/Controllers/AboutController.cs
@@ -36,7 +36,7 @@ namespace SignalRApi.Controllers
             return Ok("Hakkımda Kısmı Başarılı Bir Şekilde Eklendi");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteAbout(int id)
         {
             var value = _aboutService.TGetByID(id);
@@ -58,7 +58,7 @@ namespace SignalRApi.Controllers
             return Ok("Hakkında Alanı Güncellendi");
         }
 
-        [HttpGet("GetAbout")]
+        [HttpGet("{id}")]
         public IActionResult GetAbout(int id)
         {
             var value = _aboutService.TGetByID(id);
