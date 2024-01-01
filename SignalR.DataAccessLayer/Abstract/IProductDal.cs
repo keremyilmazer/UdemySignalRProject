aeb1a4bbb0de32bf4b08a977b4bd975154fb0172
Kerem <keremyilmazerrr@hotmail.com>
2024-01-01 16:36:50 +0300

    En Yüksek ve En Düşük Fiyatlı Ürünler

--- SignalR.DataAccessLayer/Abstract/IProductDal.cs
+++ SignalR.DataAccessLayer/Abstract/IProductDal.cs
@@ -13,5 +13,8 @@ namespace SignalR.DataAccessLayer.Abstract
         int ProductCount();
         int ProductCountByCategoryNameHamburger();
         int ProductCountByCategoryNameDrink();
+        decimal ProductPriceAvg();
+        string ProductNameByMaxPrice();
+        string ProductNameByMinPrice();
     }
 }
