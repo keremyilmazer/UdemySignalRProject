bd49579e1489d3a5c6af28884b588694286413cf
Kerem <keremyilmazerrr@hotmail.com>
2024-01-04 16:10:35 +0300

    İstatistik İşlemleri Devamı

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.BusinessLayer.Abstract;
 using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
     public class SignalRHub : Hub
     {
-        SignalRContext context = new SignalRContext();
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
 
         public async Task SendCategoryCount()
         {
-            var value = context.Categories.Count();
+            var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("RecieveCategoryCount", value);
+        }
+
+        public async Task SendProductCount()
+        {
+            var value2 = _productService.TProductCount();
+            await Clients.All.SendAsync("RecieveProductCount", value2);
         }
     }
 }
