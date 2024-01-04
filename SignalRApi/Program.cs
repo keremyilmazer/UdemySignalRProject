82bf79fbdc6fb109b82fdd3cd4ce01095b610cfd
Kerem <keremyilmazerrr@hotmail.com>
2024-01-04 15:43:01 +0300

    Menü Table Count Sayısı

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -60,6 +60,9 @@ builder.Services.AddScoped<IOrderDal, EfOrderDal>();
 builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
 builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
 
+builder.Services.AddScoped<IMenuTableService, MenuTableManager>();
+builder.Services.AddScoped<IMenuTableDal, EfMenuTableDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
