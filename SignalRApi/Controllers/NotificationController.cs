ac65c708552f947f16bd1b40046e3cb6a9e828a7
Kerem <keremyilmazerrr@hotmail.com>
2024-02-10 16:10:17 +0300

    Bildirim Durumlarını Api ile Değiştirme

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -79,5 +79,19 @@ namespace SignalRApi.Controllers
             _notificationService.TUpdate(notification);
             return Ok("Güncelleme işlemi başarıyla yapıldı");
         }
+
+        [HttpGet("NotificationStatusChangeToFalse/{id}")]
+        public IActionResult NotificationStatusChangeToFalse(int id)
+        {
+            _notificationService.TNotificationStatusChangeToFalse(id);
+            return Ok("Güncelleme yapıldı");
+        }
+
+        [HttpGet("NotificationStatusChangeToTrue/{id}")]
+        public IActionResult NotificationStatusChangeToTrue(int id)
+        {
+            _notificationService.TNotificationStatusChangeToTrue(id);
+            return Ok("Güncelleme yapıldı");
+        }
     }
 }
